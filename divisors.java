1132bd0e2e5bf070eda9b6bb1d2fbfc2c7da111b
Isaias Cordova P <igerardo0808@gmail.com>
2023-11-30 02:34:34 -0600

    divisors 1

---
+++ divisors.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class divisors {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int n;
+        n = leer.nextInt();
+
+        ArrayList<Integer> divisores = new ArrayList<Integer>();
+        for(int i = n; i>0 ;i--)
+        {
+            if(n % i == 0)
+            {
+                divisores.add(i);
+            }
+        }
+
+        // Iteramos los elementos
+        for(Integer elemento: divisores)
+        {
+            System.out.println(elemento);
+        }
+
+        leer.close();
+    }
+}
