aac6eceed106ed77a65b2a39f8eccf15022a29d7
Isaias Cordova P <igerardo0808@gmail.com>
2023-11-30 02:06:22 -0600

    ss solved

--- simplesort.java
+++ simplesort.java
@@ -18,19 +18,25 @@ public class simplesort {
         Scanner leer = new Scanner(System.in);
         ArrayList<Integer> Numbers = new ArrayList<Integer>(3);
         ArrayList<Integer> SortedNumbers = new ArrayList<Integer>();
-        
+        ArrayList<Integer> ListaRespaldo = new ArrayList<>();
+
         for(int i = 0; i<3; i++)
         {
             // Anadimos los numeros al ArrayList
             Numbers.add(leer.nextInt());
         }
 
-        int menor = Numbers.get(0); // Primer numero -> 7.
+        ListaRespaldo.add(Numbers.get(0));
+        ListaRespaldo.add(Numbers.get(1));
+        ListaRespaldo.add(Numbers.get(2));
+
+
         // int menor = 0;
         // Recorremos la lista hasta que no tenga elementos.
-        int j = 0;
         while(!Numbers.isEmpty())
         {
+            int menor = Numbers.get(0); // Primer numero -> 7.
+            int indexMenor = 0;
             // menor = j;
 
             // Primer ronda
@@ -46,23 +52,33 @@ public class simplesort {
 
             // Tercera ronda.
             // 21 < 21
-            if(Numbers.get(j) < menor || Numbers.size() == 1)
-            {
-                // Si entra a este bucle mandamos a una nueva lista.
-                menor = Numbers.get(0);
-                // Eliminamos el elemento encontrado.
-                Numbers.remove(j);
-                // Anadimos a la nueva lista.
-                SortedNumbers.add(Numbers.get(j));
-                // Contador negativo para que al salir de if incremente a 0.
-                j = -1;
-            }
-            // Incrementamos j si no entro al bucle anterior.
-            j++;
 
-        }
-        
-        // Imprimimos el nuevo array.
-        System.out.println(SortedNumbers);
+            for(int i = 1; i < Numbers.size(); i++)
+            {
+                if(Numbers.get(i) < menor)
+                {
+                    menor = Numbers.get(i);
+                    indexMenor = i;
+                }
+            }
+
+            // Anadimos el elemento
+            SortedNumbers.add(menor);
+            Numbers.remove(indexMenor);
+         
+    }
+    // Imprimos acorde al formato solicitado.
+
+    // Lista ordenada
+    for(Integer elementoOrdenado: SortedNumbers)
+    {
+        System.out.println(elementoOrdenado);
+    }
+    System.out.println("");
+    // Lista original
+    for(Integer listaOriginal: ListaRespaldo)
+    {
+        System.out.println(listaOriginal);
+    }
     }
 }
