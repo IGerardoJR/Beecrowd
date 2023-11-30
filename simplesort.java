b30d93c9e874b7264080fe7c6979e8c1e7915167
Isaias Cordova P <igerardo0808@gmail.com>
2023-11-30 01:55:55 -0600

    simplesort 3

--- simplesort.java
+++ simplesort.java
@@ -1,6 +1,13 @@
 // BEE 1042 - Simple Sort - Level 1
 
 // 7 21 -14
+// 7 21
+// 21
+
+
+// -14 
+//  7 
+//  21
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -8,40 +15,54 @@ import java.util.Stack;
 
 public class simplesort {
     public static void main(String[] args) {
-    Scanner leer = new Scanner(System.in);
-    // Defining a stack.
+        Scanner leer = new Scanner(System.in);
+        ArrayList<Integer> Numbers = new ArrayList<Integer>(3);
+        ArrayList<Integer> SortedNumbers = new ArrayList<Integer>();
+        
+        for(int i = 0; i<3; i++)
+        {
+            // Anadimos los numeros al ArrayList
+            Numbers.add(leer.nextInt());
+        }
 
-    Stack<Integer> pilaNumeros = new Stack<Integer>();
-    ArrayList<Integer> sortedNumbers = new ArrayList<Integer>();
-    // Reading the values and pushing onto the stack.
-    for(int i = 0; i < 3; i++)
-    {
-        pilaNumeros.push(leer.nextInt());
-    }
+        int menor = Numbers.get(0); // Primer numero -> 7.
+        // int menor = 0;
+        // Recorremos la lista hasta que no tenga elementos.
+        int j = 0;
+        while(!Numbers.isEmpty())
+        {
+            // menor = j;
 
-    // Comparisions with the values of the stack.
-    int lowerValue = pilaNumeros.elementAt(0); // 7
-    int j = 0; // iterator.
-    do {
-       if(j < 3)
-       {
+            // Primer ronda
+            // 7 < 7
+            // 21 < 7
+            // -14 < 7 = true
 
-           // 7 -> 7 ? true, 7 -> 21 ? true, then 7 -> -14 ? false, then -14 is the lower.
-           if(pilaNumeros.elementAt(j) <= lowerValue)
-           {
-               lowerValue = pilaNumeros.elementAt(j);
-               sortedNumbers.add(lowerValue);
-            }else{
-                j++;
+
+            // Segunda ronda.
+            // 7 < 7
+            // 7 < 21 = true.
+
+
+            // Tercera ronda.
+            // 21 < 21
+            if(Numbers.get(j) < menor || Numbers.size() == 1)
+            {
+                // Si entra a este bucle mandamos a una nueva lista.
+                menor = Numbers.get(0);
+                // Eliminamos el elemento encontrado.
+                Numbers.remove(j);
+                // Anadimos a la nueva lista.
+                SortedNumbers.add(Numbers.get(j));
+                // Contador negativo para que al salir de if incremente a 0.
+                j = -1;
             }
-            pilaNumeros.remove(lowerValue);
-            j = 0;
-        } 
+            // Incrementamos j si no entro al bucle anterior.
+            j++;
 
-
-    } while (!pilaNumeros.empty());
-
-
-    System.out.println(sortedNumbers);
+        }
+        
+        // Imprimimos el nuevo array.
+        System.out.println(SortedNumbers);
     }
 }
