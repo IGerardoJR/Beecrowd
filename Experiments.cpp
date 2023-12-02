f36263637c34067831dee9da1d471a10f5465da8
Isaias Cordova P <igerardo0808@gmail.com>
2023-12-02 03:09:00 -0600

    Experimentals WIP 1

--- Experiments.cpp
+++ Experiments.cpp
@@ -9,21 +9,41 @@ int main(int argc, char const *argv[])
 
     // Testing the array list
     list<int> miLista = {};
-
     list<int>::iterator it;
 
-    miLista.push_front(10);
-    miLista.push_front(20);
-    miLista.push_back(30);
-    miLista.push_front(50);
-    
+    // Number of cases
+    int n;
+    scanf("%d",&n);
 
-    for(it =miLista.begin(); it != miLista.end(); it++)
+    for(int i = 0; i < n ; i++)
     {
-        cout<<*it<<endl;
+        // WIP
     }
+
+
 
     
 
     return 0;
 }
+
+
+// Datos a recolectar
+/*
+    Total de animales
+    Numero en especifico de los siguientes animales
+    {
+        Total de coelhos(conejos)
+        Total de ratos
+        Total de sapos
+    }
+    Del total de animales sacar el porcentaje de
+    {
+        coelhos(conejos)
+        ratos
+        sapos
+    }
+    
+
+
+*/
